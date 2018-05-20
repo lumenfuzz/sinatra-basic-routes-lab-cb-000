af5fcd4f562a81f24373c078567483a467b4738c
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-20 21:19:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,5 +6,14 @@ class App < Sinatra::Base
   get '/' do
     "Hello, World!"
   end
+  get '/name' do
+    "My name is (name)"
+  end
+  get '/hometown' do
+    "My hometown is (hometown)"
+  end
+  get '/favorite-song' do
+    "My favorite song is (song)"
+  end
 
 end
